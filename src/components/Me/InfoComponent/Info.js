941a4cc00c92d6d26e923ee3d39d70698378c011
quy <leuducquy@gmail.com>
2017-05-05 17:27:06 +0700

    modify date component

--- src/components/Me/InfoComponent/Info.js
+++ src/components/Me/InfoComponent/Info.js
@@ -1,9 +1,11 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import gql from 'graphql-tag';
 import { graphql, compose } from 'react-apollo';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import update from 'immutability-helper';
 import { Button } from 'react-bootstrap';
+import { Field, reduxForm } from 'redux-form';
+import DateComponent from './DateComponent';
 
 import s from './Info.scss';
 
@@ -30,7 +32,7 @@ const updateProfile = gql`mutation updateProfile ($profile: UpdateProfileInput!)
   }
   ${UserFragment}`;
 
-class Info extends React.Component {
+class Info extends Component {
 
   static propTypes = {
     profile: PropTypes.object.isRequired,
@@ -43,21 +45,31 @@ class Info extends React.Component {
       profile: {},
     };
   }
+
   componentWillReceiveProps(nextProps) {
     const { profile } = nextProps;
     if (profile && profile !== this.props.profile) {
       this.setState({ profile });
     }
   }
-  editProfile = () => {
-    const { isEdit, profile } = this.state;
+  handleOnSubmit = (values) => {
+    console.log(values);
+    const { isEdit } = this.state;
     this.setState({
       isEdit: !isEdit,
     });
-
+  }
+  handleEnableEdit = (e) => {
+    e.preventDefault();
+    const { isEdit } = this.state;
+    this.setState({
+      isEdit: !isEdit,
+    });
+    /*
     if (this.state.isEdit === true) {
       this.props.updateProfile(profile);
     }
+    */
   }
   genderChange =(e) => {
     const { profile: { picture, firstName, lastName } } = this.state;
@@ -70,41 +82,95 @@ class Info extends React.Component {
       },
     });
   }
+   
+
   render() {
-    const { address, email, gender, birthday, phone } = this.state.profile;
+    const {
+      address,
+      email,
+      gender,
+      birthday,
+      phone } = this.state.profile;
+    const { isEdit } = this.state;
+    const { handleSubmit } = this.props;
 
     return (
+      <form onSubmit={handleSubmit(this.handleOnSubmit)}>
+        <div className={s.root}>
 
-      <div className={s.root}>
+          <ul className={s.profile}>
 
-        <ul className={s.profile}>
+            <li className={s.fixfont}>
+              <i className="fa fa-mobile fa-3x " aria-hidden="true"></i>
+              <span>Số điện thoại</span>
+              {isEdit
+              ? <Field
+                name="phone"
+                component="input"
+                type="text"
+              />
+              : phone || ''
+              }
+            </li>
+            <li>
+              <i className="fa fa-address-book-o fa-2x" aria-hidden="true"></i>
+              <span>Địa Chỉ</span>
+              {isEdit && <Field name="address" component="input" type="text" value={address || ''} /> }
 
-          <li className={s.fixfont}>
-            <i className="fa fa-mobile fa-3x " aria-hidden="true"></i><span>Số điện thoại</span> {phone || ''}
-          </li>
-          <li>
-            <i className="fa fa-address-book-o fa-2x" aria-hidden="true"></i>
-            <span>Địa Chỉ</span> {address || ''}
-          </li>
-          <li>
-            <i className="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-            <span>Email </span> { email || ''}
-          </li>
-          <li>
-            <i className="fa fa-venus-mars fa-2x" aria-hidden="true"></i>
-            <span>Giới tính</span>
-            { this.state.isEdit && <input type="text" name="gender" value={gender} onChange={this.genderChange} /> }
-            {!this.state.isEdit && gender && gender}
-          </li>
-          <li>
-            <i className="fa fa-birthday-cake fa-2x" aria-hidden="true"></i>
-            <span>Ngày Sinh</span> {birthday || ''}
-          </li>
-          <li><Button onClick={this.editProfile} className={s.button} >{this.state.isEdit ? SAVE : CHANGE}</Button></li>
-        </ul>
-      </div>
-
-
+            </li>
+            <li>
+              <i className="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+              <span>Email </span>
+              {isEdit
+              && <Field
+                name="email"
+                component="input"
+                type="text"
+              />}
+              {!isEdit && email && email}
+            </li>
+            <li>
+              <i className="fa fa-venus-mars fa-2x" aria-hidden="true"></i>
+              <span>Giới tính</span>
+              {isEdit
+              && (<label>
+                <Field
+                  name="gender"
+                  component="input"
+                  type="radio" value="male"
+                />
+                  Male
+                </label>)}
+              {isEdit
+              && (<label>
+                <Field
+                  name="gender"
+                  component="input"
+                  type="radio"
+                  value="female"
+                />
+               Female
+               </label>)}
+              {!this.state.isEdit && gender && gender}
+            </li>
+            <li>
+              <i className="fa fa-birthday-cake fa-2x" aria-hidden="true"></i>
+              <span>Ngày Sinh</span>
+            {isEdit && <Field name="date" component={DateComponent} selected={new Date()} />}
+              {birthday || ''}
+            </li>
+            <li>
+              <Button
+                type={isEdit ? 'submit' : 'button'}
+                onClick={!isEdit ? this.handleEnableEdit : null}
+                className={s.button}
+              >
+                {this.state.isEdit ? SAVE : CHANGE}
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </form>
     );
   }
 }
@@ -128,6 +194,7 @@ export default compose(
          updateQueries: {
 
            mePageQuery: (previousResult, { mutationResult }) => {
+             debugger;
              const newProfile = mutationResult.data && mutationResult.data.profile;
              return update(previousResult, {
                me: {
@@ -142,4 +209,4 @@ export default compose(
      }),
    }),
 
-)(Info);
+reduxForm({ form: 'myform' }))(Info);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import user from './user';
 import chat from './chat';
 import runtime from './runtime';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 export default function createRootReducer({ apolloClient }) {
   return combineReducers({
+    form: reduxFormReducer,
     apollo: apolloClient.reducer(),
     user,
     chat,
