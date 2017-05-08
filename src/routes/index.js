5536c92a871be43a1dbc1868e131eadede9a0f97
quy <leuducquy@gmail.com>
2017-05-08 10:26:00 +0700

    modify authen me

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,7 +27,7 @@ export default {
     require('./about').default,
     require('./privacy').default,
     require('./admin').default,
-    require('./me').default,
+    requireAuth(require('./me').default),
     // Wildcard routes, e.g. { path: '*', ... } (must go last)
     require('./notFound').default,
   ],
