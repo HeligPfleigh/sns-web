52b155c0849e5d81221276537a12c6cea719266c
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-06-13 22:07:23 +0700

    user can like and unlike post

--- src/routes/building/Building.js
+++ src/routes/building/Building.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import gql from 'graphql-tag';
+import update from 'immutability-helper';
 import { graphql, compose } from 'react-apollo';
 import { Grid } from 'react-bootstrap';
 import s from './Building.scss';
@@ -33,13 +34,13 @@ const loadBuildingQuery = gql`
   }
 ${Feed.fragments.post}`;
 
-function doNothing (evt) {
+function doNothing(evt) {
   evt.preventDefault();
 }
 
 class Building extends Component {
   render() {
-    const { data: { loading, building, me } } = this.props;
+    const { data: { loading, building, me }, likePost, unlikePost } = this.props;
     console.log(building);
     return (
       <Grid>
@@ -55,7 +56,8 @@ class Building extends Component {
           new post here <br />
           { building && building.posts && <FeedList
             feeds={building ? building.posts : []}
-            likePostEvent={doNothing}
+            likePostEvent={likePost}
+            unlikePostEvent={unlikePost}
             userInfo={me}
             loadMoreComments={doNothing}
             createNewComment={doNothing}
@@ -75,6 +77,8 @@ Building.propTypes = {
     }),
     loading: PropTypes.bool.isRequired,
   }).isRequired,
+  likePost: PropTypes.func.isRequired,
+  unlikePost: PropTypes.func.isRequired,
   // buildingId: PropTypes.string.isRequired,
 };
 
@@ -91,6 +95,82 @@ export default compose(
       variables: {
         buildingId: props.buildingId,
       },
+    }),
+  }),
+  graphql(Feed.mutation.likePost, {
+    props: ({ mutate }) => ({
+      likePost: (postId, message, totalLikes, totalComments, user) => mutate({
+        variables: {
+          postId,
+        },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          likePost: {
+            __typename: 'PostSchemas',
+            _id: postId,
+            message,
+            user: {
+              __typename: 'UserSchemas',
+              _id: user._id,
+              username: user.username,
+              profile: user.profile,
+            },
+            totalLikes: totalLikes + 1,
+            totalComments,
+            isLiked: true,
+          },
+        },
+        updateQueries: {
+          loadBuildingQuery: (previousResult, { mutationResult }) => {
+            const updatedPost = mutationResult.data.likePost;
+            const index = previousResult.building.posts.findIndex(item => item._id === updatedPost._id);
+            return update(previousResult, {
+              building: {
+                posts: {
+                  $splice: [[index, 1, updatedPost]],
+                },
+              },
+            });
+          },
+        },
+      }),
+    }),
+  }),
+  graphql(Feed.mutation.unlikePost, {
+    props: ({ mutate }) => ({
+      unlikePost: (postId, message, totalLikes, totalComments, user) => mutate({
+        variables: { postId },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          unlikePost: {
+            __typename: 'Post',
+            _id: postId,
+            message,
+            user: {
+              __typename: 'Friend',
+              _id: user._id,
+              username: user.username,
+              profile: user.profile,
+            },
+            totalLikes: totalLikes - 1,
+            totalComments,
+            isLiked: false,
+          },
+        },
+        updateQueries: {
+          homePageQuery: (previousResult, { mutationResult }) => {
+            const updatedPost = mutationResult.data.unlikePost;
+            const index = previousResult.building.posts.findIndex(item => item._id === updatedPost._id);
+            return update(previousResult, {
+              building: {
+                posts: {
+                  $splice: [[index, 1, updatedPost]],
+                },
+              },
+            });
+          },
+        },
+      }),
     }),
   }),
 )(Building);
