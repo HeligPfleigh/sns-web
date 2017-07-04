c87588dc9b9d56a5c0a3f635b093d93149fd5cd1
Linh Le Duc <linh.le@mttjsc.com>
2017-07-04 18:43:40 +0700

    fixed messages are continuous lines without breaking

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import _ from 'lodash';
+import moment from 'moment';
 import classnames from 'classnames';
 import Link from '../Link';
 import s from './Message.scss';
@@ -9,25 +10,33 @@ class Message extends React.Component {
   static propTypes = {
     message: PropTypes.object.isRequired,
     members: PropTypes.array.isRequired,
+    isShowDate: PropTypes.bool.isRequired,
   };
 
   render() {
-    const { message, members } = this.props;
+    const { message, members, isShowDate } = this.props;
     const messageValues = Object.values(message)[0];
     const user = _.find(members, o => o.uid === messageValues.user);
     const picture = user && user.profile && user.profile.picture;
     const isOwner = user === members[0];
     return (
-      <div className={classnames(s.root, { [s.rootOwner]: isOwner })} >
-        <div className={s.chatUser}>
-          <Link to={`/user/${user.id}`}>
-            <img alt="Message" src={picture || '/tile.png'} />
-          </Link>
+      <span>
+        {
+          isShowDate === true && <div className={s.dateView}>
+            <span>{ moment(messageValues && messageValues.timestamp).format('HH:mm DD/MM/YYYY') }</span>
+          </div>
+        }
+        <div className={classnames(s.root, { [s.rootOwner]: isOwner })} >
+          <div className={s.chatUser}>
+            <Link to={`/user/${user.id}`}>
+              <img alt="Xem thông tin" src={picture || '/tile.png'} />
+            </Link>
+          </div>
+          <div className={s.message}>
+            <div dangerouslySetInnerHTML={{ __html: messageValues && messageValues.message }} />
+          </div>
         </div>
-        <div className={s.message}>
-          <div dangerouslySetInnerHTML={{ __html: messageValues && messageValues.message }} />
-        </div>
-      </div>
+      </span>
     );
   }
 }
