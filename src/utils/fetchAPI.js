502cf3055584ac681b8a74b3ec3a866d6bf0df80
Linh Le Duc <linh.le@mttjsc.com>
2017-07-13 11:39:50 +0700

    Create http fetch data util

---
+++ src/utils/fetchAPI.js
@@ -0,0 +1,19 @@
+const fetchAPI = async (url, data) => {
+  const response = await fetch(url, {
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    credentials: 'same-origin',
+    method: 'post',
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  const result = await response.json();
+  return result;
+};
+
+export default fetchAPI;
