c4683429ddcba4c2c4052f6b3f3a6a0bf3e48db7
Nam Hoang Van <particle4dev@gmail.com>
2017-07-29 10:33:51 +0700

    removed BuildingNotificationList, fixed fake data

--- src/routes/BuildingNotification/BuildingNotification.js
+++ src/routes/BuildingNotification/BuildingNotification.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import { graphql, compose } from 'react-apollo';
 import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import BuildingNotificationList, { BuildingNotificationItem } from '../../components/BuildingNotificationList';
+import BuildingNotificationItem from '../../components/BuildingNotificationItem';
 import Label from '../../components/Friend/Label';
 import buildingNotificationQuery from './buildingNotificationQuery.graphql';
 import s from './BuildingNotification.scss';
 
 const buildingNotification = [
   {
-    type: '#006400',
+    type: 'type01',
     date: '11:00 AM  15 tháng 5',
     title: 'Thông báo tổ chức mùng 1-6 cho các bé',
   },
   {
-    type: '#006400',
+    type: 'type01',
     date: '11:00 AM  15 tháng 4',
     title: 'Thông báo diễn tập phòng cháy chữa cháy',
   },
   {
-    type: '#FF8C00',
+    type: 'type02',
     date: '11:00 AM  01 tháng 4',
     title: 'Thông báo nộp tiền phí dịch vụ tháng 4',
   },
@@ -39,16 +39,21 @@ class BuildingNotification extends Component {
           </li>
         </FriendsList>
         } */}
-        <BuildingNotificationList>
-          <li style={{ backgroundColor: '#FF8C00', height: '10px', marginBottom: '5px' }}></li>
-          {
-            buildingNotification.map(notification =>
-              <BuildingNotificationItem
-                notification={notification}
-              />,
-            )
-          }
-        </BuildingNotificationList>
+        <div className={s.buildingNotificationList}>
+          <ul>
+            <li style={{ backgroundColor: '#FF8C00', height: '10px', marginBottom: '5px' }}></li>
+            {
+              buildingNotification.map(notification =>
+                <BuildingNotificationItem
+                  notification={notification}
+                />,
+              )
+            }
+          </ul>
+          <div className={s.buildingNotificationListFooter}>
+            Xem thêm
+          </div>
+        </div>
       </div>
     );
   }
