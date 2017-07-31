8e74b91f41854d70da4d4cae42b19a8fcc342c78
Linh Le Duc <linh.le@mttjsc.com>
2017-07-31 13:39:51 +0700

    Change mutaion share post on Home Page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -12,6 +12,7 @@ import FriendSuggestions from '../FriendSuggestions';
 import NewPost from '../../components/NewPost';
 import CommentList from '../../components/Comments/CommentList';
 import FeedList, { Feed } from '../../components/Feed';
+import { PUBLIC } from '../../constants';
 import s from './Home.scss';
 
 const homePageQuery = gql`query homePageQuery ($cursor: String) {
@@ -341,8 +342,11 @@ export default compose(
   }),
   graphql(Feed.mutation.sharingPost, {
     props: ({ mutate }) => ({
-      sharingPost: postId => mutate({
-        variables: { _id: postId },
+      sharingPost: (postId, privacy) => mutate({
+        variables: {
+          _id: postId,
+          privacy: privacy || PUBLIC,
+        },
         update: (store, { data: { sharingPost } }) => {
           // Read the data from our cache for this query.
           let data = store.readQuery({ query: homePageQuery });
