74b10ce844bba19cd4a62e4f72493d1bf7551dd2
hoang <particle4dev@lehoangs-MacBook-Pro.local>
2017-08-03 20:36:36 +0700

    update css

--- src/routes/userApprovalPage/UserApprovalPage.js
+++ src/routes/userApprovalPage/UserApprovalPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { graphql, compose } from 'react-apollo';
-import { Grid, Row, Col, ControlLabel } from 'react-bootstrap';
+import { Grid, Row, Col, ControlLabel, Clearfix } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import userApprovalPageQuery from './userApprovalPageQuery.graphql';
 import s from './UserApprovalPage.scss';
@@ -27,84 +27,88 @@ class UserApprovalPage extends Component {
     return (
       <div>
         {resident &&
-          <Grid className={s.profile}>
-            <Row className={s.backBuildingRequestTab}>
-              <Col md={8} sm={12} xs={12}>
-                <div onClick={this.backBuildingRequestTab}>
-                  <i className="fa fa-chevron-left" aria-hidden="true"></i>
-                  <h4>Quay lại trang</h4>
-                </div>
-                <h3>THÔNG TIN</h3>
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-user-circle" aria-hidden="true"></i>
-                <ControlLabel htmlFor="name">Name</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                {resident.profile.lastName} {resident.profile.firstName}
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-mobile" aria-hidden="true"></i>
-                <ControlLabel htmlFor="phoneNumber">Số điện thoại</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                094 559 36 37
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-building" aria-hidden="true"></i>
-                <ControlLabel htmlFor="address">Địa chỉ</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                Căn hộ A204, Tòa tháp Tây Hacorplaza
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-envelope-o" aria-hidden="true"></i>
-                <ControlLabel htmlFor="email">Email</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                abc@gmail.com
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-venus-mars" aria-hidden="true"></i>
-                <ControlLabel htmlFor="gender">Giới Tính</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                {resident.profile.gender === 'male' ? 'Nam' : 'Nữ'}
-              </Col>
-            </Row>
-            <Row className={s.profileInfo}>
-              <Col sm={2}>
-                <i className="fa fa-birthday-cake" aria-hidden="true"></i>
-                <ControlLabel htmlFor="email">Ngày sinh</ControlLabel>
-              </Col>
-              <Col sm={10} className={s.profileRight}>
-                16 tháng 5 năm 1980
-              </Col>
-            </Row>
+          <Grid>
             <Row>
-              <Col md={8} sm={12} xs={12}>
-                <button
-                  onClick={this.acceptUser}
-                  className={s.buttonAccept}
-                >
-                  Đồng ý
-                </button>
-                <button
-                  onClick={this.rejectUser}
-                  className={s.buttonCancel}
-                >
-                  Từ chối
-                </button>
+              <Col md={8} sm={12} xs={12} className={s.profile}>
+                <Row className={s.backBuildingRequestTab}>
+                  <Col md={8} sm={12} xs={12}>
+                    <div onClick={this.backBuildingRequestTab}>
+                      <i className="fa fa-chevron-left" aria-hidden="true"></i>
+                      <h4>Quay lại trang</h4>
+                    </div>
+                    <h3>THÔNG TIN</h3>
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-user-circle" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="name">Name</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    {resident.profile.lastName} {resident.profile.firstName}
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-mobile" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="phoneNumber">Số điện thoại</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    094 559 36 37
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-building" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="address">Địa chỉ</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    Căn hộ A204, Tòa tháp Tây Hacorplaza
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-envelope-o" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="email">Email</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    abc@gmail.com
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-venus-mars" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="gender">Giới Tính</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    {resident.profile.gender === 'male' ? 'Nam' : 'Nữ'}
+                  </Col>
+                </Row>
+                <Row className={s.profileInfo}>
+                  <Col sm={3}>
+                    <i className="fa fa-birthday-cake" aria-hidden="true"></i>
+                    <ControlLabel htmlFor="email">Ngày sinh</ControlLabel>
+                  </Col>
+                  <Col sm={9} className={s.profileRight}>
+                    16 tháng 5 năm 1980
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={8} sm={12} xs={12}>
+                    <button
+                      onClick={this.acceptUser}
+                      className={s.buttonAccept}
+                    >
+                      Đồng ý
+                    </button>
+                    <button
+                      onClick={this.rejectUser}
+                      className={s.buttonCancel}
+                    >
+                      Từ chối
+                    </button>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Grid>
