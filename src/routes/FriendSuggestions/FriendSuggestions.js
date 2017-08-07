48b8bb8a3bcb32980db0a9662eada3a3a5871271
Nam Hoang Van <particle4dev@gmail.com>
2017-08-07 15:40:30 +0700

    remove console.log in file FriendSuggestions

--- src/routes/FriendSuggestions/FriendSuggestions.js
+++ src/routes/FriendSuggestions/FriendSuggestions.js
@@ -12,11 +12,10 @@ import s from './FriendSuggestions.css';
 class FriendSuggestions extends Component {
   render() {
     const { data: { loading, resident } } = this.props;
-    console.log(resident.friendSuggestions.edges.length);
     return (
       <div>
         {loading && <h1 style={{ textAlign: 'center' }}>Đang tải dữ liệu</h1>}
-        {!loading && resident.friendSuggestions.edges.length > 0 && <FriendsList>
+        {!loading && resident && resident.friendSuggestions && resident.friendSuggestions.edges.length > 0 && <FriendsList>
           <li style={{ paddingLeft: '10px' }}>
             <Label label="Giới thiệu bạn bè" />
           </li>
