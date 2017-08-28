e17d8b64549da1cc7011f3139e15ea1de8c1f51f
Linh Le Duc <linh.le@mttjsc.com>
2017-08-28 08:25:14 +0700

    FeeManagement Upload Fee Page

--- src/routes/BuildingManagement/FeeManagement/UploadPage/index.js
+++ src/routes/BuildingManagement/FeeManagement/UploadPage/index.js
@@ -2,36 +2,45 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { Grid, Row, Col } from 'react-bootstrap';
 import MediaQuery from 'react-responsive';
-import Loading from '../../components/Loading';
-import s from './FeeManagement.scss';
-import getFeeTypes from './getFeeTypes.graphql';
-import {
-  UploadFeeFile,
-  Menu,
-} from '../../components/FeeManagement';
-import config from '../../config';
 
-class FeeManagement extends Component {
+import s from './styles.scss';
+import Menu from '../../Menu/Menu';
+import Loading from '../../../../components/Loading';
+import UploadFeeFile from '../../../../components/FeeManagement';
+
+const getFeeTypes = gql`query {
+  getFeeTypes {
+    code
+    name
+  }
+}`;
+
+class FeeUploadPage extends Component {
   state= {
     loading: false,
   }
-
   render() {
     const { loading } = this.state;
     const { buildingId, user, feeTypes } = this.props;
+
     return (
       <Grid>
         <Loading show={loading} full>Đang tải ...</Loading>
         <Row className={s.containerTop30}>
           <MediaQuery minDeviceWidth={992} values={{ deviceWidth: 1600 }}>
             <Col md={3} smHidden xsHidden>
-              <Menu user={user} />
+              <Menu
+                user={user}
+                parentPath={`/management/${buildingId}`}
+                pageKey="fee_management>fee_upload"
+              />
             </Col>
           </MediaQuery>
-          <Col md={9} sm={12} xs={12}>
+          <Col md={9}>
             <UploadFeeFile
               buildingId={buildingId}
               feeTypes={feeTypes}
@@ -43,17 +52,17 @@ class FeeManagement extends Component {
   }
 }
 
-FeeManagement.propTypes = {
+FeeUploadPage.propTypes = {
   user: PropTypes.object.isRequired,
   feeTypes: PropTypes.array.isRequired,
   buildingId: PropTypes.string.isRequired,
 };
 
 export default compose(
+  withStyles(s),
   connect(state => ({
     user: state.user,
   })),
-  withStyles(s),
   graphql(getFeeTypes, {
     options: () => ({
       fetchPolicy: 'network-only',
@@ -62,5 +71,4 @@ export default compose(
       feeTypes: data.getFeeTypes,
     }),
   }),
-)(FeeManagement);
-
+)(FeeUploadPage);
