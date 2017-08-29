79e97bc709c0f9af2ab1029295d195b0766c08c8
Linh Le Duc <linh.le@mttjsc.com>
2017-08-29 21:38:14 +0700

    Implement UI Fee Page Complete

--- src/routes/BuildingManagement/FeeManagement/ReportPage/FeeList.js
+++ src/routes/BuildingManagement/FeeManagement/ReportPage/FeeList.js
@@ -1,46 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Table from 'rc-table';
+import includes from 'lodash/includes';
+import { Pagination, Clearfix } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './styles.scss';
-
-const colsTable = [{
-  title: 'Tên chủ hộ',
-  dataIndex: 'apartment.owner.profile.fullName',
-  key: 'apartment.owner.profile.fullName',
-  width: '25%',
-}, {
-  title: 'Căn hộ', dataIndex: 'apartment.name', key: 'apartment.name', width: '15%',
-}, {
-  title: 'Tổng tiền', dataIndex: 'totals', key: 'totals', width: '25%',
-}, {
-  title: 'Tình trạng', dataIndex: 'status', key: 'status', width: '15%',
-}, {
-  title: 'Hành động', dataIndex: '', key: 'x', render: this.renderAction,
-}];
-
-const colsSubTable = [{
-  title: 'Loại phí', dataIndex: 'type.name', key: 'type.name', width: '30.5%',
-}, {
-  title: 'Thành tiền', dataIndex: 'total', key: 'total', width: '23.5%',
-}, {
-  title: 'Trạng thái', dataIndex: 'status', key: 'status', width: '20%',
-}];
-
-const colsTableFilter = [{
-  title: 'Tên chủ hộ',
-  dataIndex: 'apartment.owner.profile.fullName',
-  key: 'apartment.owner.profile.fullName',
-  width: '25%',
-}, {
-  title: 'Căn hộ', dataIndex: 'apartment.name', key: 'apartment.name', width: '10%',
-}, {
-  title: 'Loại phí', dataIndex: 'totals', key: 'type.name', width: '25%',
-}, {
-  title: 'Thành tiền', dataIndex: 'totals', key: 'totals', width: '25%',
-}, {
-  title: 'Tình trạng', dataIndex: 'status', key: 'status', width: '15%',
-}];
 
 class FeeList extends Component {
   constructor(props) {
@@ -58,18 +22,88 @@ class FeeList extends Component {
   }
 
   expandedRowRender = record => (
-    <Table rowKey="_id" columns={colsSubTable} data={record.detail} />
+    <Table
+      rowKey="_id"
+      showHeader={false}
+      columns={this.configCols(true)}
+      data={record.detail}
+    />
   );
 
   handleClick = (index) => {
     console.log(index);
   }
 
-  renderAction = (o, row, index) => (<a href="#" onClick={() => this.handleClick(index)}>Chi tiết</a>);
+  handlePageSelect = (pageNum) => {
+    this.props.changePage(pageNum);
+  }
+
+  configCols = (isSubTable) => {
+    if (isSubTable) {
+      return [{
+        title: 'Số phòng', dataIndex: '', key: 'a', width: '20.1%',
+      }, {
+        title: 'Loại phí', dataIndex: 'type.name', key: 'type.name', width: '21.7%',
+      }, {
+        title: 'Thành tiền', dataIndex: 'total', key: 'total', width: '26.7%',
+      }, {
+        title: 'Trạng thái', dataIndex: 'status', key: 'status', width: '15.7%',
+      }, {
+        title: '', dataIndex: '', key: 'x', render: this.viewFeeDetail,
+      }];
+    }
+
+    return [{
+      title: 'Số phòng', dataIndex: 'apartment.name', key: 'apartment.name', width: '20%',
+    }, {
+      title: 'Loại phí', dataIndex: '', key: 'a', width: '20%',
+    }, {
+      title: 'Thành tiền', dataIndex: 'totals', key: 'totals', width: '25%',
+    }, {
+      title: 'Trạng thái', dataIndex: 'status', key: 'status', width: '15%',
+    }, {
+      title: '', dataIndex: '', key: 'x', render: this.renderAction,
+    }];
+  }
+
+  viewFeeDetail = (o, row, index) => (
+    <a href="#" onClick={() => this.handleClick(index)}>{'Chi tiết >>'}</a>
+  );
+
+  renderAction = (o, row, index) => {
+    if (includes(this.state.expandedRowKeys, o._id)) {
+      return (<a
+        href="#" onClick={(evt) => {
+          evt.preventDefault();
+          this.handleClick(index);
+        }}
+      >
+        Xem thêm
+        <i
+          aria-hidden="true"
+          className="fa fa-caret-down"
+          style={{ marginLeft: '3px' }}
+        ></i>
+      </a>);
+    }
+    return (<a
+      href="#" onClick={(evt) => {
+        evt.preventDefault();
+        this.handleClick(index);
+      }}
+    >
+      Xem thêm
+      <i
+        aria-hidden="true"
+        className="fa fa-caret-right"
+        style={{ marginLeft: '3px' }}
+      ></i>
+    </a>);
+  }
 
   render() {
     const { expandedRowKeys } = this.state;
-    const { treeMode, dataSource } = this.props;
+    const { treeMode, pagination, dataSource } = this.props;
 
     return (
       <span>
@@ -79,7 +113,7 @@ class FeeList extends Component {
             data={dataSource || []}
             expandIconAsCell
             expandRowByClick
-            columns={colsTable}
+            columns={this.configCols(false)}
             emptyText="Không có thông tin hiển thị"
             expandedRowKeys={expandedRowKeys}
             expandedRowRender={this.expandedRowRender}
@@ -91,9 +125,25 @@ class FeeList extends Component {
             rowKey="_id"
             emptyText="Dữ liệu tìm kiếm không tồn tại"
             data={dataSource || []}
-            columns={colsTableFilter}
+            columns={this.configCols(false)}
           />
         }
+        { (pagination.totalPage > 1) &&
+          <div className="pull-right">
+            <Pagination
+              maxButtons={5}
+              prev={pagination.totalPage > 5}
+              next={pagination.totalPage > 5}
+              first={pagination.totalPage > 5}
+              last={pagination.totalPage > 5}
+              ellipsis={pagination.totalPage > 5}
+              items={pagination.totalPage}
+              activePage={pagination.currentPage}
+              onSelect={this.handlePageSelect}
+            />
+          </div>
+        }
+        <Clearfix />
       </span>
     );
   }
@@ -101,6 +151,8 @@ class FeeList extends Component {
 
 FeeList.propTypes = {
   treeMode: PropTypes.bool.isRequired,
+  pagination: PropTypes.object.isRequired,
+  changePage: PropTypes.func.isRequired,
   dataSource: PropTypes.array.isRequired,
 };
 
