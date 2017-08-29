8b897422087074e4f4d545c9bfd261a08dba03cb
Linh Le Duc <linh.le@mttjsc.com>
2017-08-29 15:15:50 +0700

    551 Hide links navigation which haven't yet direct to any page and deploy a demo product

--- src/components/EventsComponents/EventMenu/EventMenu.js
+++ src/components/EventsComponents/EventMenu/EventMenu.js
@@ -3,22 +3,24 @@ import PropTypes from 'prop-types';
 import { compose } from 'react-apollo';
 import { Button } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+import history from '../../../core/history';
 import s from './EventMenu.scss';
-import MenuVertical from '../../MenuVertical';
+// import MenuVertical from '../../MenuVertical';
 import CreateEventModal from '../../CreateEventModal';
 
 const currentDate = new Date();
 
-const menus = [{
-  text: 'Sự kiện',
-  key: 'event',
-}, {
-  text: 'Quá khứ',
-  key: 'past',
-}, {
-  text: 'Lịch',
-  key: 'calendar',
-}];
+// const menus = [{
+//   text: 'Sự kiện',
+//   key: 'event',
+// }, {
+//   text: 'Quá khứ',
+//   key: 'past',
+// }, {
+//   text: 'Lịch',
+//   key: 'calendar',
+// }];
 
 class EventMenu extends Component {
   constructor(props) {
@@ -44,6 +46,10 @@ class EventMenu extends Component {
     });
   }
 
+  goEventsHome = () => {
+    history.push('/events');
+  }
+
   render() {
     const { showCreateEventModal } = this.state;
     return (
@@ -52,14 +58,16 @@ class EventMenu extends Component {
           show={showCreateEventModal}
           closeModal={this.closeModal}
         />
-        <i className={`fa fa-calendar-o ${s.iconCalender}`} aria-hidden="true">
-          <span>{currentDate.getDate()}</span>
-        </i>
-        <span className={s.eventTitle}>Sự Kiện</span>
-        <MenuVertical
+        <span className={s['events-home']} onClick={this.goEventsHome}>
+          <i className={`fa fa-calendar-o ${s.iconCalender}`} aria-hidden="true">
+            <span>{currentDate.getDate()}</span>
+          </i>
+          <span className={s.eventTitle}>Sự Kiện</span>
+        </span>
+        { /* <MenuVertical
           menus={menus}
           onItemSelected={(idx) => { this.onMenuSelected(idx); }}
-        />
+        /> */}
         <Button
           onClick={this.onCreateEvent}
           className={s.btnCreateEvent}
