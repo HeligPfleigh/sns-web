1bf4352b5c0d787c5b6d8838071f1bfef512fc74
Nam Hoang Van <particle4dev@gmail.com>
2017-08-31 19:20:29 +0700

    create an announcement

--- src/routes/BuildingManagement/AnnouncementPage/index.js
+++ src/routes/BuildingManagement/AnnouncementPage/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { graphql, compose } from 'react-apollo';
 import { connect } from 'react-redux';
-import { compose } from 'react-apollo';
+import { reset } from 'redux-form';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { Grid, Row, Col } from 'react-bootstrap';
 import MediaQuery from 'react-responsive';
 import Loading from '../../../components/Loading';
 import Menu from '../Menu/Menu';
+import NewAnnouncementForm from './NewAnnouncementForm';
+import { openAlertGlobal } from '../../../reducers/alert';
+import createNewBuildingAnnouncementMutation from './createNewBuildingAnnouncementMutation.graphql';
 import s from './styles.scss';
 
 class Announcement extends Component {
   state= {
     loading: false,
   }
+
+  submit = (evt, values) => {
+    const { message, description } = values;
+    this.props.createNewBuildingAnnouncement(message, description)
+    .then(() => {
+      this.props.resetForm();
+      this.props.openAlertGlobalAction({
+        message: 'Bạn đã đăng thông báo thành công',
+        open: true,
+        autoHideDuration: 0,
+      });
+    }).catch((error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
+
   render() {
     const { loading } = this.state;
     const { buildingId, user } = this.props;
@@ -33,7 +53,13 @@ class Announcement extends Component {
           <Col md={9} sm={12} xs={12}>
             <div className={s.container}>
               <Col md={12} className={s.contentMain}>
-                <h1>Trang thông báo chung</h1>
+                <div className={s.header}>
+                  <i className="fa fa-bullhorn" aria-hidden="true"></i>
+                  <span className={s.headerTitle}>Thông báo khác</span>
+                </div>
+                <NewAnnouncementForm
+                  onSubmit={this.submit}
+                />
               </Col>
             </div>
           </Col>
@@ -45,7 +71,10 @@ class Announcement extends Component {
 
 Announcement.propTypes = {
   user: PropTypes.object.isRequired,
+  createNewBuildingAnnouncement: PropTypes.func.isRequired,
   buildingId: PropTypes.string.isRequired,
+  openAlertGlobalAction: PropTypes.func,
+  resetForm: PropTypes.func,
 };
 
 export default compose(
@@ -53,4 +82,25 @@ export default compose(
     user: state.user,
   })),
   withStyles(s),
-)(Announcement);
+  graphql(createNewBuildingAnnouncementMutation, {
+    props: ({ ownProps, mutate }) => ({
+      createNewBuildingAnnouncement: (message, description) => mutate({
+        variables: {
+          input: {
+            buildingId: ownProps.buildingId,
+            announcementInput: {
+              message,
+              description,
+            },
+          },
+        },
+      }),
+    }),
+  }),
+)(connect(
+  null,
+  dispatch => ({
+    openAlertGlobalAction: data => dispatch(openAlertGlobal(data)),
+    resetForm: () => dispatch(reset('newAnnouncementForm')),
+  }),
+)(Announcement));
