c20e4d48323900f0d4cceaa6fcbb7b713c28c436
Nam Hoang Van <particle4dev@gmail.com>
2017-09-22 11:02:37 +0700

    allow user unfriend, accept friend, reject friend on search page

--- src/routes/search/Search.js
+++ src/routes/search/Search.js
@@ -7,12 +7,25 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import searchPageQuery from './searchPageQuery.graphql';
 import sendFriendRequestMutation from '../FriendSuggestions/sendFriendRequestMutation.graphql';
 import cancelFriendRequestedMutation from './cancelFriendRequestedMutation.graphql';
+import sendUnfriendRequestMutation from './sendUnfriendRequestMutation.graphql';
+import acceptFriendOnSearchPageMutation from './acceptFriendOnSearchPageMutation.graphql';
+import rejectFriendOnSearchPageMutation from './rejectFriendOnSearchPageMutation.graphql';
 import SearchItem from './SearchItem';
 import s from './Search.scss';
 
 class Search extends Component {
   render() {
-    const { data: { search }, query } = this.props;
+    const {
+      data: {
+        search,
+      },
+      query,
+      sendFriendRequest,
+      cancelFriendRequested,
+      sendUnfriendRequest,
+      acceptFriendAction,
+      rejectFriendAction,
+    } = this.props;
     return (
       <Grid>
         <Row className={s.top20}>
@@ -27,8 +40,11 @@ class Search extends Component {
                     <SearchItem
                       key={idRandom()}
                       dataUser={item}
-                      sendFriendRequest={this.props.sendFriendRequest}
-                      cancelFriendRequested={this.props.cancelFriendRequested}
+                      sendFriendRequest={sendFriendRequest}
+                      cancelFriendRequested={cancelFriendRequested}
+                      sendUnfriendRequest={sendUnfriendRequest}
+                      acceptFriendAction={acceptFriendAction}
+                      rejectFriendAction={rejectFriendAction}
                     />
                 ))}
               </div>
@@ -47,6 +63,9 @@ Search.propTypes = {
   query: PropTypes.object,
   sendFriendRequest: PropTypes.func.isRequired,
   cancelFriendRequested: PropTypes.func.isRequired,
+  sendUnfriendRequest: PropTypes.func.isRequired,
+  acceptFriendAction: PropTypes.func.isRequired,
+  rejectFriendAction: PropTypes.func.isRequired,
 };
 
 Search.defaultProps = {
@@ -74,6 +93,27 @@ export default compose(
   graphql(cancelFriendRequestedMutation, {
     props: ({ mutate }) => ({
       cancelFriendRequested: _id => mutate({
+        variables: { _id },
+      }),
+    }),
+  }),
+  graphql(sendUnfriendRequestMutation, {
+    props: ({ mutate }) => ({
+      sendUnfriendRequest: _id => mutate({
+        variables: { _id },
+      }),
+    }),
+  }),
+  graphql(acceptFriendOnSearchPageMutation, {
+    props: ({ mutate }) => ({
+      acceptFriendAction: _id => mutate({
+        variables: { _id },
+      }),
+    }),
+  }),
+  graphql(rejectFriendOnSearchPageMutation, {
+    props: ({ mutate }) => ({
+      rejectFriendAction: _id => mutate({
         variables: { _id },
       }),
     }),
